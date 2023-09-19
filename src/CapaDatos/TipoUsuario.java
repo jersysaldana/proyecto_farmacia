c73ce725381be4e0e289a9769bfcbff42a219a44
jersy saldaña <73138952jersy@gimail.com>
2023-09-18 20:02:31 -0500

    avance proyecto_farmacia

---
+++ src/CapaDatos/TipoUsuario.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaDatos;
+
+/**
+ *
+ * @author labor
+ */
+public class TipoUsuario {
+    private int idtipousuario;
+    private String tuNombre;
+
+    public TipoUsuario() {
+    }
+
+    
+    public TipoUsuario(int idtipousuario, String tuNombre) {
+        this.idtipousuario = idtipousuario;
+        this.tuNombre = tuNombre;
+    }
+
+    public int getIdtipousuario() {
+        return idtipousuario;
+    }
+
+    public void setIdtipousuario(int idtipousuario) {
+        this.idtipousuario = idtipousuario;
+    }
+
+    public String getTuNombre() {
+        return tuNombre;
+    }
+
+    public void setTuNombre(String tuNombre) {
+        this.tuNombre = tuNombre;
+    }
+    
+    
+    
+}
